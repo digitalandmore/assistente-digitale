eb91e9033d072121c80013b3436bce03f43c6157
micelibenedetto800 <micelibenedetto800@gmail.com>
2025-08-24 20:28:47 +0200

    fix

--- services/supabaseService.js
+++ services/supabaseService.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
     'https://uczhhyjytabcqhznozgc.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjemhoeWp5dGFiY3Foem5vemdjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NTc2MTY5MCwiZXhwIjoyMDcxMzM3NjkwfQ.gU4ib-v8oNV4rif8g-6RsNHhLD9BZQkhKUlzm_cW3N4'
+    process.env.SUPABASE_SECRET_API_KEY
 )
 
 export default supabase;
